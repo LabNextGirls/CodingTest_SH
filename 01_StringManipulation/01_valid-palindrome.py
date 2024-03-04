3969b3a87d7519c6ab07ee9d5db1b2a146537160
SoHyeon Kim <happythgus89@gmail.com>
2024-03-04 20:39:33 +0900

    Update 01_valid-palindrome.py

--- 01_StringManipulation/01_valid-palindrome.py
+++ 01_StringManipulation/01_valid-palindrome.py
@@ -1,1 +1,21 @@
+import re
 
+def isPalindrome(str):
+    new_str = re.sub(r"[^a-zA-Z]", "", str)
+    new_str = new_str.lower()
+
+    str_list = list(new_str)
+    str_list.reverse()
+
+    if (list(new_str) == str_list):
+        isPalindrome = True
+    else:
+        isPalindrome = False
+
+    return isPalindrome
+
+first = "A man, a plan, a canal: Panama"
+second = "race a car"
+
+print(isPalindrome(first))
+print(isPalindrome(second))
