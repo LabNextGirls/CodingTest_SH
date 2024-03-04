70547e5e6aeefac7a95d58ec723d0fbead0de9b5
SoHyeon Kim <happythgus89@gmail.com>
2024-03-05 00:34:54 +0900

    05_group-anagrams.py

--- 01_StringManipulation/05_group-anagrams.py
+++ 01_StringManipulation/05_group-anagrams.py
@@ -1,2 +1,23 @@
 # https://leetcode.com/problems/group-anagrams/description/
 
+import re
+from typing import List
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+# [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+def groupAnagrams(strs):
+    word_list = []
+
+    for word in strs:
+        word_list.append(list(word))
+        print(word_list)
+
+    groups = []
+    for word in word_list:
+        for j in range(len(word_list) - 1):
+            if (word_list[j] == word):
+                
+    return groups
+
+groupAnagrams(strs)
