7ccfc30ce8a737b5395807f835247f8a1b14533e
SoHyeon Kim <happythgus89@gmail.com>
2024-03-04 21:26:16 +0900

    03_reorder-log-files.py

--- 01_StringManipulation/03_reorder-log-files.py
+++ 01_StringManipulation/03_reorder-log-files.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/reorder-data-in-log-files/
 
+from typing import List
+
 def reorderLogFiles(logs):
     reordered_logs = []
 
@@ -17,3 +19,19 @@ logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art ze
 print(reorderLogFiles(logs))
 
 # output = ['let1 art can', 'let2 own kit dig', 'let3 art zero', 'dig1 8 1 5 1', 'dig2 3 6']
+
+# 답안 
+def reorderLogFiles(self, logs: List[str]) -> List[str]:
+    letters, digits = [], []
+    
+    for log in logs:
+        if log.split()[1].isdigit():
+            digits.append(log)
+        else:
+            letters.append(log)
+
+    # 2개의 키를 람다 표현식으로 정렬
+    # 문자가 동일한 경우에만 앞 번호 순으로 정렬
+    letters.sort(key=lambda x: (x.split( )[1:], x.split()[0]))
+     
+    return letters + digits
