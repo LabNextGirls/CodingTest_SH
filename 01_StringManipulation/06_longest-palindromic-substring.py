1408df343e2359c338a4d7518f46da8e73a11491
SoHyeon Kim <happythgus89@gmail.com>
2024-03-10 17:43:48 +0900

    06_longest-palindromic-substring

--- 01_StringManipulation/06_longest-palindromic-substring.py
+++ 01_StringManipulation/06_longest-palindromic-substring.py
@@ -1,2 +1,19 @@
 # https://leetcode.com/problems/longest-palindromic-substring/description/
 
+# 거꾸로 읽어도 제대로 읽는 것과 같은 말 
+
+def longestPalindrome(s):
+    ans_list = []
+    reverse = s[::-1]
+
+    for i in range(len(s)):
+        for j in range(len(s)):
+            if (i < j):
+                if (s[i:j:] in reverse):
+                    print(s[i:j:])
+                    ans_list.append(s[i:j:])
+
+    return max(ans_list)
+
+s = "babad"
+print(longestPalindrome(s))
