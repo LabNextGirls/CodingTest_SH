c950461dd8e2a8ad85b359dee81d8abbbd5147e1
SohyeonKim-dev <happythgus89@gmail.com>
2024-03-13 21:09:39 +0900

    [FEAT] two-sum

--- 02_Array/01_two-sum.py
+++ 02_Array/01_two-sum.py
@@ -1,1 +1,35 @@
 # https://leetcode.com/problems/two-sum/description/
+
+def twoSum(nums, target):
+    indexes = []
+
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if (nums[i] + nums[j] == target):
+                indexes.append([i, j])
+
+    return indexes[0::2]
+
+nums = [2,7,11,15]
+target = 9
+print(twoSum(nums, target))
+
+# in을 활용한 풀이 
+# target - n 값이 존재하는지 check 
+
+def twoSum2(nums, target):
+    indexes = []
+
+    for i, n in enumerate(nums):
+        complement = target - n
+
+        # 탐색 범위를 i 번째 인덱스 이후로 
+        if complement in nums[i + 1:]:
+            indexes.append(nums.index(n), nums[i+1:].index(complement) + i + 1 )
+
+    return indexes[0::2]
+
+nums = [2,7,11,15]
+target = 9
+print(twoSum(nums, target))
+
---
+++ 02_Array/02_trapping-rain-water.py
@@ -0,0 +1,11 @@
+# 높이를 입력받아 비 온 후 얼마나 많은 물이 쌓일 수 있는지 계산하라
+# https://leetcode.com/problems/trapping-rain-water/description/
+
+def trap(height):
+    max_water = 0
+
+
+    return max_water
+
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+print(trap(height))
