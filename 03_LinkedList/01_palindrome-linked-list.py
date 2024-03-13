827982dbe2bcb0b3cb9328f87b7f1559992c17a6
SohyeonKim-dev <happythgus89@gmail.com>
2024-03-13 22:53:50 +0900

    [ADD] Linked List

---
+++ 03_LinkedList/01_palindrome-linked-list.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/palindrome-linked-list/description/
+
+def isPalindrome(head):
+    boolean = True 
+
+    return boolean
+
+head = [1,2,2,1]
+print(isPalindrome(head))
