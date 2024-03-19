14b9de63731e1ec041b299e5bb25c2b831086dbd
SohyeonKim-dev <happythgus89@gmail.com>
2024-03-19 21:22:21 +0900

    [FEAT] palindrome linked list

--- 03_LinkedList/01_palindrome-linked-list.py
+++ 03_LinkedList/01_palindrome-linked-list.py
@@ -1,9 +1,22 @@
 # https://leetcode.com/problems/palindrome-linked-list/description/
+# Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val    # 해당 노드의 값
+#         self.next = next  # 포인터 역할
 
 def isPalindrome(head):
-    boolean = True 
 
-    return boolean
+    num = len(head)
+    for i in range(int(num/2)):
+        if (head[i] == head[num-i-1]):
+            continue
+        else:
+            return False
+
+    return True
 
 head = [1,2,2,1]
 print(isPalindrome(head))
