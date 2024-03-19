4a4ffd8c92cf8e967b7f6f66fd0f04f1b44e100f
SohyeonKim-dev <happythgus89@gmail.com>
2024-03-19 22:35:26 +0900

    [ADD] swap nodes in pairs

---
+++ 03_LinkedList/05_swap-nodes-in-pairs.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/swap-nodes-in-pairs/description/
+# 연결 리스트를 입력받아 페어 단위로 스왑하라
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        ret_list = ListNode()
+        
+        return ret_list.next
