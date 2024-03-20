401f1ef63a9fb447ae861c06dc6442fbf1c05c2f
SohyeonKim-dev <happythgus89@gmail.com>
2024-03-20 16:38:48 +0900

    [ADD] stack and queue

---
+++ 04_StackQueue/01_valid-parentheses.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/valid-parentheses/description/
+# 괄호로 된 입력값이 올바른지 판별하라. (245 page)
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
