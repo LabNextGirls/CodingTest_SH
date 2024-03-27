1b361fca0c1ab8b1e8359c116b24f75bf00491bf
SohyeonKim-dev <happythgus89@gmail.com>
2024-03-27 16:35:26 +0900

    [FEAT] valid parentheses

--- 04_StackQueue/01_valid-parentheses.py
+++ 04_StackQueue/01_valid-parentheses.py
@@ -1,10 +1,27 @@
 # https://leetcode.com/problems/valid-parentheses/description/
 # 괄호로 된 입력값이 올바른지 판별하라. (245 page)
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+# determine if the input string is valid.
 
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+def isValid(s):
+    stack = []
+    table = { ')': '(',
+              '}': '{',
+              ']': '['
+            }
+    
+    for char in s:
+        if char not in table: 
+            # ( { [ 에 해당 
+            stack.append(char)
+
+            # ) } ] 에 해당
+            # 무조건 둘이 쌍으로 붙어있어야 함 
+            
+        elif not stack or table[char] != stack.pop():
+            return False
         
+    return len(stack) == 0
+
+s = "()[]}"
+print(isValid(s))
