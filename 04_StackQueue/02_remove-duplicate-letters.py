27462c111bee50be8e9fa0a5e35696f1ecfe5b35
SohyeonKim-dev <happythgus89@gmail.com>
2024-03-28 14:30:13 +0900

    [FEAT] remove duplicated letters

--- 04_StackQueue/02_remove-duplicate-letters.py
+++ 04_StackQueue/02_remove-duplicate-letters.py
@@ -4,4 +4,15 @@
 # You must make sure your result is the smallest in lexicographical order among all possible results.
 
 def removeDuplicateLetters(s):
+    queue = []
+
+    for char in s:
+        if char not in queue:
+            queue.append(char)
+
+    queue = sorted(queue)
+    print(queue)
+    return ''.join(queue)
     
+s = "cbacdcbc"
+print(removeDuplicateLetters(s))
